dd2f1d20c33034d704c40e67d2243391cf262494
C0nijc <conijocus@gmail.com>
2025-09-18 18:55:05 +0200

    ejecuto ejr en linux

--- mascotas/obj/Debug/net9.0/mascotas.AssemblyInfo.cs
+++ mascotas/obj/Debug/net9.0/mascotas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mascotas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8214426e3bd85201d20e117fb65c95812834411")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+890302449613e404ebb9771b3611f8dbb15d6be8")]
 [assembly: System.Reflection.AssemblyProductAttribute("mascotas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mascotas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
