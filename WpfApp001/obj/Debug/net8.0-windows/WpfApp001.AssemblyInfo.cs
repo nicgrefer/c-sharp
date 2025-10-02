e6809553f559eb18c8a60b46d132580a8a4a8d0d
coni <nicolas.jove.cubillo@gregoriofer.com>
2025-10-02 12:21:35 +0200

    Cargo interfaz grafica

--- WpfApp001/obj/Debug/net8.0-windows/WpfApp001.AssemblyInfo.cs
+++ WpfApp001/obj/Debug/net8.0-windows/WpfApp001.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp001")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f2c06fa7a2d13dcdba4eb23ec64fb1cfc40e7b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e2e398fe1048ee517affc244d1174c4f40d421a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp001")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp001")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ WpfEjre001/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfEjre001
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
