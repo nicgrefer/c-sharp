bd65f6fbad94110fcf4788ff7e4eb6d7c36c0407
Nicolás <nicolas.jove.cubillo@gregoriofer.com>
2025-10-14 11:06:02 +0200

    ejemplo imagenes
    lista de imagen que al pulsar se abra en ventana emergente

---
+++ WpfAppDockpanelFotos/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppDockpanelFotos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void lbAves_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbAves.SelectedItem != null) // Recomendable poner porque aveces se carga esta funcion sin que se haya pulsado nada
+                                             // de esta forma verificamos si es nulo o no
+            {
+                ListBox lb = (ListBox)sender;
+                ListBoxItem lbi = (ListBoxItem)lbAves.SelectedItem;
+                Image img = (Image)lbi.Content;
+                WindowDetalle ventanaDetalle = new WindowDetalle(img);
+                ventanaDetalle.ShowDialog();
+                // al regresar de la ventana detalle, deseleccionamos el item seleccionado
+                lb.SelectedItem = null; 
+            }
+        }
+    }
+}
---
+++ WpfAppDockpanelFotos/WindowDetalle.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppDockpanelFotos
+{
+    /// <summary>
+    /// Lógica de interacción para WindowDetalle.xaml
+    /// </summary>
+    public partial class WindowDetalle : Window
+    {
+        public WindowDetalle(Image image)
+        {
+            InitializeComponent();
+            imgDetalle.Source = image.Source;
+        }
+
+        private void imgDetalle_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
