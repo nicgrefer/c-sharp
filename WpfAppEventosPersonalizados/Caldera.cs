830a971c87ad1b98ca5ae1fc6b864f3e10a36a8e
Nicolás <nicolas.jove.cubillo@gregoriofer.com>
2025-11-04 10:44:57 +0100

    Initial project setup for WpfAppEventosPersonalizados
    Added all source files, project configuration, and build outputs for the WpfAppEventosPersonalizados WPF application. This includes main application files, solution and project files, publish profiles, and initial build artifacts for both .NET 6.0 and .NET 8.0 targets.

---
+++ WpfAppEventosPersonalizados/Caldera.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfAppEventosPersonalizados
+{
+    internal class Caldera
+    {
+        public event EventHandler<CalderaArgumentosEvento> 
+            CambioTemperatura;
+        public event EventHandler<CalderaArgumentosEvento> 
+            CambioEstado;
+        public event EventHandler StopCaldera;
+        internal enum Estados : int
+        {
+            Correcto = 0,
+            Alerta = 1,
+            Peligro = 2
+        }
+
+        private int temperatura;
+        public int Temperatura
+        {
+            private get { return temperatura; }
+            set
+            {
+                temperatura = value;
+                if (temperatura > 99) 
+                  { StopCaldera?.Invoke(this, EventArgs.Empty); }
+                if (temperatura < 60) 
+                  { Estado = Estados.Correcto; }
+                else if (temperatura < 80) 
+                  { Estado = Estados.Alerta; }
+                else 
+                  { Estado = Estados.Peligro; }
+                CambioTemperatura?.Invoke
+                    (this, new CalderaArgumentosEvento(
+                        temperatura, DateTime.Now, estado));
+            }
+        }
+
+        private Estados estado;
+        public Estados Estado
+        {
+            get { return estado; }
+            private set
+            {
+                if (value != Estado)
+                {
+                    estado = value;
+                    CambioEstado?.Invoke
+                        (this, new CalderaArgumentosEvento(
+                            temperatura, DateTime.Now, estado));
+                }
+            }
+        }
+    }
+
+    internal class CalderaArgumentosEvento : EventArgs
+    {
+        public CalderaArgumentosEvento(int t, DateTime h, Caldera.Estados estado)
+        {
+            GradosC = t;
+            Hora = h;
+            EstadoCaldera = estado;
+        }
+        public int GradosC { get; set; }
+        public DateTime Hora { get; }
+        public Caldera.Estados EstadoCaldera { get; set; }
+    }
+
+}
---
+++ WpfAppEventosPersonalizados/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfAppEventosPersonalizados
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Caldera caldera = new Caldera();
+        Color[] colores = { Colors.Green, Colors.Gold, Colors.Red }; //Correcto,Alerta,Peligro
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            IniciarCaldera();
+        }
+
+        private void IniciarCaldera()
+        {
+            lbInforme.Items.Add("Inicio " + DateTime.Now.ToLongTimeString());
+            caldera.CambioTemperatura += caldera2_CambioTemperatura;
+            caldera.CambioEstado += caldera_CambioEstado;
+            caldera.StopCaldera += Caldera_StopCaldera;
+        }
+
+        private void Caldera_StopCaldera(object sender, EventArgs e)
+        {
+            lbInforme.Items.Add("STOP " + DateTime.Now.ToLongTimeString());
+            caldera.CambioTemperatura -= caldera2_CambioTemperatura;
+            caldera.CambioEstado -= caldera_CambioEstado;
+            caldera.StopCaldera -= Caldera_StopCaldera;
+        }
+
+        void caldera_CambioEstado(object sender, CalderaArgumentosEvento e)
+        {
+            lbInforme.Items.Add($"{e.EstadoCaldera,10}\t{e.Hora.ToLongTimeString()}");
+        }
+
+        void caldera2_CambioTemperatura(object sender, CalderaArgumentosEvento e)
+        {
+            txtbTemperatura.Text = e.GradosC.ToString("00");
+            txtbTemperatura.Background = new SolidColorBrush(colores[(int)e.EstadoCaldera]);
+        }
+
+        private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            caldera.Temperatura = (int)e.NewValue;
+        }
+    }
+}
