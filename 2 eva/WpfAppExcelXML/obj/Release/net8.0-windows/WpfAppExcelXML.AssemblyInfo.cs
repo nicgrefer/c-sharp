cbee053fce791da5c362329ce56cc4fba6f5dd50
Nicolás <nicolas.jove.cubillo@gregoriofer.com>
2025-11-27 10:27:35 +0100

    ejemplo creacción ficheros excel

--- 2 eva/WpfAppExcelXML/obj/Release/net8.0-windows/WpfAppExcelXML.AssemblyInfo.cs
+++ 2 eva/WpfAppExcelXML/obj/Release/net8.0-windows/WpfAppExcelXML.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfAppExcelXML")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8b2011ed1388b11e62f9863530ba146373e04b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfAppExcelXML")]
